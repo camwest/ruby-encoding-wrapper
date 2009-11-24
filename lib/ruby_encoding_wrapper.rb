025ad47ff7eb8ad402b2d09f0f148a1f08f3a228
Mikhailov Anatoly <me@railsgeek.com>
2009-11-24 19:53:16 +0600

    request send workflow

--- lib/ruby_encoding_wrapper.rb
+++ lib/ruby_encoding_wrapper.rb
@@ -11,14 +11,17 @@ class RubyEncodingWrapper
   def initialize(user_id=nil, user_key=nil)
     @user_id = user_id
     @user_key = user_key
-    @url = 'manage.encoding.com:80'
+    @url = 'manage.encoding.com'
 
   end
 
   def request_encoding(action=nil, source=nil, format={})
-    #{ :size, :bitrate, :audio_bitrate, :audio_sample_rate, :audio_channels_number, :framerate, :two_pass, :cbr, :deinterlacing, :destination, :add_meta
+    #{ :size, :bitrate, :audio_bitrate, :audio_sample_rate,
+    #:audio_channels_number, :framerate, :two_pass, :cbr,
+    #:deinterlacing, :destination, :add_meta
 
-    xml = Builder::XmlMarkup.new(:target=>STDOUT, :indent=>2)
+    xml = Builder::XmlMarkup.new :indent=>2
+    xml.instruct! 
     xml.query do |q|
       q.userid(@user_id)
       q.userid @user_id
@@ -28,16 +31,13 @@ class RubyEncodingWrapper
       q.format format
     end
 
-
-    request_send(xml)
+    request_send(xml.target!)
   end
 
 
   private
     def request_send(xml)
-      url = URI.parse(@url)      
-      Net::HTTP.start(url.host, url.port) do |http|
-        response = http.post(url.path, "xml=#{xml}")
-      end
+      http = Net::HTTP.new(@url)
+      response = http.post(@url, "xml=#{xml}")
     end
 end
