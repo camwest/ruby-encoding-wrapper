f0b72dd616be83ed18c717623bdc23f9530ca8dc
Mikhailov Anatoly <me@railsgeek.com>
2009-11-24 19:36:31 +0600

    request_send - 50%

--- lib/ruby_encoding_wrapper.rb
+++ lib/ruby_encoding_wrapper.rb
@@ -1,8 +1,8 @@
 require 'rubygems'
 require 'uri'
 require 'net/http'
-require 'rexml/document'
 require 'builder'
+require 'rexml/document'
 
 
 class RubyEncodingWrapper
@@ -11,16 +11,16 @@ class RubyEncodingWrapper
   def initialize(user_id=nil, user_key=nil)
     @user_id = user_id
     @user_key = user_key
-    @url = 'http://manage.encoding.com:80'
+    @url = 'manage.encoding.com:80'
 
   end
 
   def request_encoding(action=nil, source=nil, format={})
     #{ :size, :bitrate, :audio_bitrate, :audio_sample_rate, :audio_channels_number, :framerate, :two_pass, :cbr, :deinterlacing, :destination, :add_meta
 
-    builder = Builder::XmlMarkup.new(:target=>STDOUT, :indent=>2)
-    builder.query do |q|
-      q.userid(@user_id);
+    xml = Builder::XmlMarkup.new(:target=>STDOUT, :indent=>2)
+    xml.query do |q|
+      q.userid(@user_id)
       q.userid @user_id
       q.userkey @user_key
       q.action action
@@ -34,10 +34,10 @@ class RubyEncodingWrapper
 
 
   private
-  def request_send(xml)
-    url = URI.parse('http://localhost:3000/someservice/')
-    request = Net::HTTP::Post.new(url.path)
-    request.body = "<?xml version='1.0' encoding='UTF-8'?><somedata><name>Test Name 1</name><description>Some data for Unit testing</description></somedata>"
-    response = Net::HTTP.start(url.host, url.port) {|http| http.request(request)}
-  end
+    def request_send(xml)
+      url = URI.parse(@url)      
+      Net::HTTP.start(url.host, url.port) do |http|
+        response = http.post(url.path, "xml=#{xml}")
+      end
+    end
 end
