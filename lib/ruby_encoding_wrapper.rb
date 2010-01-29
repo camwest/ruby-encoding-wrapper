bcc5f65cdd65e8914b03689ce35e14ce920a5198
Cameron Westland <camwest@camimac.local>
2010-01-29 09:41:32 -0500

    - Added constants for commonly used commands - Added optional notify argument to request_encoding - request_encoding now yields a block with an XML object to add format   information - Added request_status(media_id) method which returns a hash of the   current status and the time remaining

--- lib/ruby_encoding_wrapper.rb
+++ lib/ruby_encoding_wrapper.rb
@@ -4,10 +4,23 @@ require 'net/http'
 require 'builder'
 require 'rexml/document'
 
+module EncodingActions
+  ADD_MEDIA = "AddMedia"
+  GET_STATUS = "GetStatus"
+end
+
+module EncodingStatusType
+  NEW = "New"
+  WAITING = "Waiting for encoder"
+  PROCESSING = "Processing"
+  SAVING = "Saving"
+  FINISHED = "Finished"
+  ERROR = "Error"
+end
 
 class RubyEncodingWrapper
   attr_reader :user_id, :user_key, :url
-  
+
   def initialize(user_id=nil, user_key=nil)
     @user_id = user_id
     @user_key = user_key
@@ -15,7 +28,7 @@ class RubyEncodingWrapper
 
   end
 
-  def request_encoding(action=nil, source=nil, format={})
+  def request_encoding(action=nil, source=nil, notify_url=nil)
     #{ :size, :bitrate, :audio_bitrate, :audio_sample_rate,
     #:audio_channels_number, :framerate, :two_pass, :cbr,
     #:deinterlacing, :destination, :add_meta
@@ -23,15 +36,40 @@ class RubyEncodingWrapper
     xml = Builder::XmlMarkup.new :indent=>2
     xml.instruct! 
     xml.query do |q|
-      q.userid(@user_id)
-      q.userid @user_id
+      q.userid  @user_id
       q.userkey @user_key
-      q.action action
-      q.source source
-      q.format format
+      q.action  action
+      q.source  source
+      q.notify  notify_url
+      q.format { |f|
+        yield f
+      }
     end
 
-    request_send(xml.target!)
+    response = request_send(xml.target!)
+    document = REXML::Document.new(response.body)
+    document.root.elements["MediaID"][0].to_s.to_i
+  end
+
+  def request_status(media_id)
+    xml = Builder::XmlMarkup.new :indent=>2
+    xml.instruct!
+    xml.query do |q|
+      q.userid    @user_id
+      q.userkey   @user_key
+      q.action    EncodingActions::GET_STATUS
+      q.mediaid   media_id
+    end
+
+    response = request_send(xml.target!)
+
+    document = REXML::Document.new(response.body)
+    root = document.root
+
+    status = { 
+      :message => root.elements["status"][0].to_s, 
+      :time_left => root.elements["time_left"][0].to_s.to_i
+    }
   end
 
 
@@ -40,8 +78,10 @@ class RubyEncodingWrapper
       url = URI.parse(@url)
       request = Net::HTTP::Post.new(url.path)
       request.form_data = { :xml => xml }
+
       response = Net::HTTP.new(url.host, url.port).start { |http|
         http.request(request)
       }
     end
+
 end
